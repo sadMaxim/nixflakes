d4d22d75b833ff7a2fb00f617f8c03a66ceeb5c1
sadMaxim <dresvyannikovm@gmail.com>
2022-06-08 19:53:44 +0000

    basic shell

--- python/torchGPU/flake.nix
+++ python/torchGPU/flake.nix
@@ -1,11 +1,19 @@
 {
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+  };
 
-  outputs = { self }: {
+  outputs = { self,nixpkgs }: 
+    let 
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in {
 
     foo = "bar";
-    #packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+    packages.x86_64-linux.hello = pkgs.hello;
+    devShell.x86_64-linux = pkgs.mkShell {buildInputs = [self.packages.x86_64-linux.hello pkgs.cowsay]; }; 
 
     #defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
+    };
 
-  };
+  
 }
