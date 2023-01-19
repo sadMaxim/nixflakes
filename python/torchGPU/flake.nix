2ceea1a15e4e1f940040d26142698b1a04046bff
sadMaxim <dresvyannikovm@gmail.com>
2023-01-19 17:21:44 +0000

    back python shell to cuda again

--- python/torchGPU/flake.nix
+++ python/torchGPU/flake.nix
@@ -1,7 +1,8 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = github:nixos/nixpkgs/6dccdc458512abce8d19f74195bb20fdb067df50;
     utils.url = "github:numtide/flake-utils";
+    nixgl.url = "github:guibou/nixGL";
   };
 
   outputs = { self, nixpkgs, utils }:
@@ -9,27 +10,27 @@
       let
         pkgs = import nixpkgs {
           inherit system;
-          config.cudaSupport = true;
+          #config.cudaSupport = true;
           config.allowUnfree = true;
+          overlays = [nixgl.overlay];
         };
         inherit (pkgs.cudaPackages) cudatoolkit;
         inherit (pkgs.linuxPackages) nvidia_x11;
         python = pkgs.python39;
-        pythonEnv = pkgs.poetry2nix.mkPoetryEnv {
-          inherit python;
-          projectDir = ./.;
-          preferWheels = true;
-        };
-      in
-      {
-        devShell = pkgs.mkShell {
-          buildInputs = [pythonEnv nvidia_x11 cudatoolkit];
-          shellHook = ''
+        torch = pkgs.python39Packages.torchWithoutCuda;
+        torchCuda = pkgs.python39Packages.torchWithCuda;
+        shellHook = ''
             export CUDA_PATH=${cudatoolkit.lib}
             export LD_LIBRARY_PATH=${cudatoolkit.lib}/lib:${nvidia_x11}/lib
             export EXTRA_LDFLAGS="-l/lib -l${nvidia_x11}/lib"
             export EXTRA_CCFLAGS="-i/usr/include"
           '';
+      in
+      {
+        devShell = pkgs.mkShell {
+          #buildInputs = [nvidia_x11 cudatoolkit];
+          buildInputs = [torch];
+          #inherit shellHook;  
         };
       }; in with utils.lib; eachSystem defaultSystems out;
 }
